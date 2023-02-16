7797bf4a417fe5709a8aec70197176277071f476
neha-madhusudhan <115139265+neha-madhusudhan@users.noreply.github.com>
2023-02-16 08:56:28 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,8 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "My SRN is PES1UG20CS263";
+  cout << "My SRN is PES1UG20CS263\n";
+  cout << "My name is Neha\n";
+  cout << "The above line indicates that I have made changes to the code\n";
   return 0;
 }
