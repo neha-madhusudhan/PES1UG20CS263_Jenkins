29a64cd37a06903d6564240811c16c5ab022a8a6
neha-madhusudhan <115139265+neha-madhusudhan@users.noreply.github.com>
2023-02-16 09:00:28 +0530

    Create file.cpp

---
+++ file.cpp
@@ -0,0 +1,12 @@
+//File: file.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, This is Jenkins Pipeline\n";
+  cout << "Jenkins Pipeline is a combination of plugins that support the integration and implementation of continuous delivery pipelines using Jenkins.\n";
+  cout << "I have successfully built and run\n";
+  cout << "My SRN is PES1UG20CS263\n";
+  cout << "My name is Neha\n";
+  return 0;
+}
